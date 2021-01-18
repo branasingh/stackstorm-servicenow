273529fc94aeae9f5e1aca249bfbb9b9910354e7
Buddhika Ranasinghe <62392182+branasingh@users.noreply.github.com>
2021-01-18 02:23:31 -0500

    Override request_params
    Override request_params

--- actions/get.py
+++ actions/get.py
@@ -4,7 +4,8 @@ from lib.actions import BaseAction
 class GetAction(BaseAction):
     def run(self, table, query, request_params):
         s = self.client
-        r = s.query(table=table, query=query, request_params=request_params)  # pylint: disable=no-member
+        s.request_params = request_params
+        r = s.query(table=table, query=query)  # pylint: disable=no-member
         response = r.get_all()  # pylint: disable=no-member
         output = []
         for each_item in response:
