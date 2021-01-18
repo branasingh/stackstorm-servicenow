5b558f8b48a92f34b8d37be99b6d6efc532c9826
Buddhika Ranasinghe <62392182+branasingh@users.noreply.github.com>
2021-01-18 01:47:56 -0500

    Add request_params
    Add request_params

--- actions/get.py
+++ actions/get.py
@@ -2,9 +2,9 @@ from lib.actions import BaseAction
 
 
 class GetAction(BaseAction):
-    def run(self, table, query):
+    def run(self, table, query, request_params):
         s = self.client
-        r = s.query(table=table, query=query)  # pylint: disable=no-member
+        r = s.query(table=table, query=query, request_params=request_params)  # pylint: disable=no-member
         response = r.get_all()  # pylint: disable=no-member
         output = []
         for each_item in response:
