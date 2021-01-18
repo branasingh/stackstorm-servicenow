0e659d493dde4563ddbb49408dffefe6d8e8e00e
Buddhika Ranasinghe <62392182+branasingh@users.noreply.github.com>
2021-01-18 02:36:18 -0500

    Changed request_params to parameters.add_custom
    Changed request_params to parameters.add_custom

--- actions/get.py
+++ actions/get.py
@@ -4,7 +4,7 @@ from lib.actions import BaseAction
 class GetAction(BaseAction):
     def run(self, table, query, request_params):
         s = self.client
-        s.request_params = request_params
+        s.parameters.add_custom(request_params)
         r = s.query(table=table, query=query)  # pylint: disable=no-member
         response = r.get_all()  # pylint: disable=no-member
         output = []
